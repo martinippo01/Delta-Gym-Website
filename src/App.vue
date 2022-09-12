78ed27df253a2df718f900c3a20df101c94fb127
Marco444 <elscili444.code@gmail.com>
2022-09-12 08:56:52 -0300

    npm build

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
 <script lang="ts">
 
   import Vue from "vue";
-  import VideoLanding from "@/components/VideoLanding.vue";
-  import TextLanding from "@/components/TextLanding.vue";
+  import VideoLanding from "@/components/landing/VideoLanding.vue";
+  import TextLanding from "@/components/landing/TextLanding.vue";
 
   export default Vue.extend({
     name: "App",
