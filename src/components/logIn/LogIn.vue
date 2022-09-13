1172ca0983e1a83dbaafa42d174caf6c8e1da93d
laucha12 <41192791+laucha12@users.noreply.github.com>
2022-09-13 13:33:43 -0300

    color theme

---
+++ src/components/logIn/LogIn.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      HOLA COMO VA
+    </v-app-bar>
+    <v-main class="fondo">
+
+
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "LogIn",
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,4 +3,14 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    theme:{
+        themes:{
+            light:{
+                fondo: '#1E1E1E', // #E53935
+                primary: '#CFFFB3', // #FFCDD2
+                secondary: '#4b4b4b', // #3F51B5
+            },
+        },
+    }
+});
