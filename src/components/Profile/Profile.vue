59e35a6685981ad2e3973cca63e75940afaec931
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2022-09-20 09:19:35 -0300

    Working on 'Profile.vue'

---
+++ src/components/Profile/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <div>
+      <h1>delta</h1>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "ProfilePage",
+}
+</script>
+
+<style scoped>
+  div{background-color: #1e1e1e}
+  h1{
+    color: #CFFFB3;
+    font-family: "Bebas Neue";
+  }
+</style>
