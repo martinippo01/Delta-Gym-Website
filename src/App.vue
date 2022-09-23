3cc1f3c68faa451fce15f0b9db09b6a909c87a65
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2022-09-22 21:37:46 -0300

    Working on 'Profile.vue'

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <template>
   <div>
-    <sing-up> </sing-up>
+    <ProfilePage> </ProfilePage>
   </div>
 </template>
 
 <script lang="ts">
 
   import Vue from "vue";
-  import LogIn from "@/components/logIn/LogIn.vue";
-  import singUp from "@/components/logIn/SingUp.vue"
+  import ProfilePage from "@/components/Profile/Profile.vue";
+  import Vuetify from 'vuetify/lib';
+
+
+
   export default Vue.extend({
     name: "App",
-    components: { singUp},
+    components: { ProfilePage},
     data: () => ({
     }),
   });
+
+  const vuetify = new Vuetify({
+    theme: {
+      themes: {
+        light: {
+          primary: '#3f51b5',
+          secondary: '#b0bec5',
+          accent: '#8c9eff',
+          error: '#b71c1c',
+        },
+      },
+    },
+  })
+
 </script>
 
-<style>
-html {
-  overflow-y: hidden !important;
-}
-</style>
--- src/components/Profile/Profile.vue
+++ src/components/Profile/Profile.vue
@@ -1,19 +1,99 @@
 <template>
-  <v-app>
-    <div>
-      <h1>delta</h1>
-    </div>
-  </v-app>
+  <div id="app">
+    <v-app>
+      <TopBar></TopBar>
+      <v-main class="background ">
+
+        <h1 class="d-flex justify-center align-center">my profile</h1>
+
+        <v-col class="d-flex justify-center align-center background">
+          <v-avatar size="200">
+            <img
+                src="@/assets/Lionel_Messi_20180626.jpg"
+                alt="MESSI"
+            >
+          </v-avatar>
+        </v-col>
+
+        <v-col class="d-flex justify-center align-center background">
+          <v-btn
+              class="tmp"
+              :disabled="loading"
+              color="primary"
+              outlined
+
+          >
+            CHANGE PROFILE PICTURE
+          </v-btn>
+        </v-col>
+
+        <v-col class="d-flex justify-center align-center background">
+          <v-sheet
+              color="white"
+           elevation="1"
+           max-height="1000"
+           rounded
+           max-width="500">
+            <v-container>
+              <v-row justify="end">
+                <v-text-field  class="pt-10 pr-10 pl-10" label="Name"/>
+              </v-row>
+              <v-row justify="end">
+                <v-text-field  class="pt-5 pr-10 pl-10" label="Surname" />
+              </v-row>
+              <v-row justify="end">
+                <v-col >
+                  <v-text-field  class="pt-5 pr-10 pl-10" label="Age" />
+                </v-col>
+                <v-col >
+                  <v-text-field  class="pt-5 pr-10 pl-10" label="Phone" />
+                </v-col>
+              </v-row>
+              <v-row justify="end">
+                <v-text-field  class="pt-5 pr-10 pl-10" label="Email" />
+              </v-row>
+              <v-row justify="center">
+                <v-col >
+                <v-btn
+                    :disabled="loading"
+                    color="primary"
+                    text
+                    class="temp"
+
+                >
+                  DISCARD CHANGES
+                </v-btn>
+                </v-col>
+                <v-col >
+                  <v-btn
+                    :disabled="loading"
+                    color="primary"
+                       outlined
+                       class="temp"
+
+                >
+                  SAVE CHANGES
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-sheet>
+        </v-col>
+      </v-main>
+    </v-app>
+  </div>
 </template>
 
 <script>
+import TopBar from "@/components/logIn/TopBar";
+
 export default {
-  name: "ProfilePage",
+  name: "ProfilePage",  components: {TopBar},
 }
 </script>
 
 <style scoped>
-  div{background-color: #1e1e1e}
+  div{background-color: #4b4b4b}
   h1{
     color: #CFFFB3;
     font-family: "Bebas Neue";
