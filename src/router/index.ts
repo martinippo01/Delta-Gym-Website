37b598602196ec27a1d6ca6b9c000a58710c678b
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2022-10-06 16:42:33 -0300

    minor changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -64,6 +64,15 @@ const routes: Array<RouteConfig> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/CreateRoutineView.vue"),
   },
+  {
+    path: "/VerifyEmail",
+    name: "VerifyEmail",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/VerifyEmail.vue"),
+  },
 ];
 
 const router = new VueRouter({
--- src/views/VerifyEmail.vue
+++ src/views/VerifyEmail.vue
@@ -1,50 +1,41 @@
 <template>
-
   <v-app>
     <TopBar select="singUp"/>
     <v-main class="background">
       <v-container >
         <v-row class="mt-1">
           <v-col class="d-flex justify-center align-center" >
-            <v-img src="@/assets/Logo.png"   max-height="116" max-width="124"/>
+            <v-img src="@/assets/Logo.png" max-height="116" max-width="124"/>
           </v-col>
         </v-row>
         <v-row class="mt-3" >
           <v-col  class="d-flex justify-center align-center" style="align-items: center" >
             <v-sheet
-                color="secondary"
-                height="475"
-                width="500"
-                rounded="xl"
-                style="align-items: center"
+              color="secondary"
+              height="475"
+              width="500"
+              rounded="xl"
+              style="align-items: center"
 
             >
               <v-container>
-                <v-row justify="end">
-                  <v-text-field class="pt-7 pr-10 pl-10" label="Full Name" dark="dark" color="primary" v-model="fullName"/>
+
+                <v-row justify="center" style="padding: 10px; font-family: 'Bebas Neue'; color: #cfffb3">
+                  <h1>Verify email</h1>
                 </v-row>
+
                 <v-row justify="end">
-                  <v-text-field class="pt-3 pr-10 pl-10" label="Email" dark="dark" color="primary" v-model="email"/>
-                </v-row>
-                <v-row justify="end">
-                  <v-text-field class="pt-3 pr-10 pl-10" label="Password" dark="dark" color="primary" v-model="password" type="password"/>
-                </v-row>
-                <v-row justify="end">
-                  <v-text-field class="pt-3 pr-10 pl-10" label="Confirm Password" dark="dark" color="primary" v-model="re_password" type="password"/>
-                </v-row>
-                <v-row justify="end">
-                  <v-text-field class="pt-3 pr-10 pl-10" label="Age" dark="dark" color="primary" @keypress="filter" v-model="age"/>
+                  <v-text-field class="pt-7 pr-10 pl-10" label="Code" dark="dark" color="primary" @keypress="filter" v-model="code"/>
                 </v-row>
                 <v-row justify="center">
-                  <router-link to="/VerifyEmail">
+                  <router-link style="text-decoration: none; color: inherit; padding: 10px" to="/VerifyEmail">
                     <v-btn
-                        :disabled="loading"
-                        color="primary"
-                        plain
-                        class="temp"
-
+                      :disabled="loading"
+                      color="primary"
+                      plain
+                      class="temp"
                     >
-                      REGISTER
+                      VERIFY
                     </v-btn>
                   </router-link>
                 </v-row>
@@ -58,7 +49,7 @@
                 <v-row justify="center" class="mt-4">
                   <router-link to="/login">
                     <a>
-                          Log In
+                      Log In
                     </a>
                   </router-link>
                 </v-row>
@@ -67,25 +58,19 @@
           </v-col>
         </v-row>
       </v-container>
-
     </v-main>
   </v-app>
-
-
 </template>
 
 <script>
-import TopBar from "@/components/logIn/TopBar.vue";
+
+import TopBar from "@/components/logIn/TopBar";
 export default {
-  name: "SingUp",
-  components:{TopBar},
+  name: "VerifyEmail",
+  components: { TopBar  },
   data(){
     return{
-      fullName:'',
-      email:'',
-      password:'',
-      re_password:'',
-      age:''
+      code:''
     }
   },
   methods:{
@@ -100,7 +85,7 @@ export default {
       }
     }
   }
-}
+};
 </script>
 
 <style scoped>
