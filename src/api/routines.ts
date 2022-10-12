52bc3da11586c17af3f3bd1dc03b0ad6d90f27be
Marco444 <elscili444.code@gmail.com>
2022-10-12 18:51:23 -0300

    minor changes

--- src/api/routines.ts
+++ src/api/routines.ts
@@ -45,10 +45,6 @@ class RoutinesApi {
       null
     );
   }
-}
-  static async addCycle(rutineId:number,name:string,detail:string,type:string,order:number,repetitions:number){
-    return await Api.post(this.getUrl(`${rutineId.toString()}/cycles`),true,new Cycle(name,detail,type,order,repetitions),null)
-  }
   static async getCycle(rutineId : number){
     const aux = await Api.get(this.getUrl(`${rutineId.toString()}/cycles`),true,null);
     return aux.content;
